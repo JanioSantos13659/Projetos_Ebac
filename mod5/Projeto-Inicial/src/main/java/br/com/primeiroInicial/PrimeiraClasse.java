7808626c67c8c750e15b3cf4c277856261658a26
Janio Santos <janiosantos13659@gmail.com>
2022-12-27 20:48:59 -0300

    Envio do primeiro projeto para o repositorio git

---
+++ mod5/Projeto-Inicial/src/main/java/br/com/primeiroInicial/PrimeiraClasse.java
@@ -0,0 +1,15 @@
+package br.com.primeiroInicial;
+
+import java.util.Scanner;
+
+public class PrimeiraClasse {
+
+    public static void main(String[] args) {
+       String nome = "José Silva";
+
+       System.out.println("Hello " + nome);
+
+
+
+    }
+}
---
+++ mod5/Projeto-Inicial/src/test/java/br/com/primeiroInicial/ProjetoInicialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.primeiroInicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoInicialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
